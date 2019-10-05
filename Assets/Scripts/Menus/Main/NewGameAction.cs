d31d20580014eca40d1b49bba2115858262137cf
Kyappy <m.gavanier@gmail.com>
2019-10-05 13:41:12 +0200

    add menu scripts

---
+++ Assets/Scripts/Menus/Main/NewGameAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menus.Main
+{
+	public class NewGameAction : MonoBehaviour
+	{
+		public void Execute()
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
