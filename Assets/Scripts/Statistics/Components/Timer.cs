84a38697a0e1df21e102574a70ecd6a02babca24
Kyappy <m.gavanier@gmail.com>
2019-10-05 13:40:43 +0200

    add statistics scripts

---
+++ Assets/Scripts/Statistics/Components/Timer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Statistics.Components
+{
+	public class Timer : MonoBehaviour
+	{
+		public float Value { get; private set; }
+
+		private void OnEnable()
+		{
+			Value = 0;
+		}
+
+		private void Update()
+		{
+			Value += Time.deltaTime;
+		}
+	}
+}
